a3a9cd6e2e6b726ee58f878ec0883bfdfb85dc64
Rabitek <tomaskralicekml@seznam.cz>
2026-03-26 10:26:32 +0000

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,12 @@
+
 #include <iostream>
-#include <string>
 
 int main()
-{
-   std::cout << "Vitejte v kalkulacce" << std::endl;
-std::cout << "Zadejte prvni cislo: ";
-float a;
-std::cin >> a;
-std::cout << "Zadejte druhe cislo: ";
-float b;
-std::cin >> b;
+{ // wwwwwwwwwwwwwwwwwwwwwwwwwww
 
-float sum = a + b;
-float difference = a - b;
-float product = a * b;
-float quotient = a / b;
+   int a;
+   a = 56;
+   std::cout << a << std::endl;
 
-std::cout << "Soucet: " << sum << std::endl;
-std::cout << "Rozdil: " << difference << std::endl;
-std::cout << "Soucin: " << product << std::endl;
-std::cout << "Podil: " << quotient << std::endl;
-std::cout << "Dekuji za pouziti kalkulacky, aplikaci ukoncite libovolnou klavesou." << std::endl;
-std::cin.get();
-std::cin.get();
-
-return 0;
+   return 0;
 }
