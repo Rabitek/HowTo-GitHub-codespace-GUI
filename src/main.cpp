cec9bab0067bbb9b2f16e66d22cead346e7c5860
Rabitek <tomaskralicekml@seznam.cz>
2026-04-23 11:32:26 +0000

    ulice lidi

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <string>
 
-double calculateRectangleArea(double width, double height)
+struct User
 {
-    double area = width * height;
-    return area;
-}
+    std::string name;
+    int age;
+    std::string street;
+};
 
+void printUser(const User& user)
+{
+    std::cout << "Name: " << user.name << std::endl;
+    std::cout << "Age: " << user.age << std::endl;
+    std::cout << "Street: " << user.street << std::endl << std::endl;
+}
 
 int main()
 {
-  std::cout << "Rectangle area is: " << calculateRectangleArea(10, 20) << " cm^2" << std::endl;
+    User user1{ "Tomas Marny", 33, "Sikma 5" };
+    User user2{ "Josef Novy", 28, "Ve Svahu 8" };
+
+    printUser(user1);
+    printUser(user2);
 
     return 0;
 }
